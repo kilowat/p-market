682a510a4697346fae22db53bda491a3c25e53d8
kilowat <kadantsev.sergey@gmail.com>
2017-03-14 12:42:34 +0300

    init

--- app/js/main.js
+++ app/js/main.js
@@ -40,4 +40,38 @@ $(document).ready(function(){
   ]
   });
 
+    $('.brands .brands-list').slick({
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll:4
+        }
+      },
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+      }
+    ]
+    });
 });
